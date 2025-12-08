d25cdcc0a33f77e7ead0ae51b8d1ccbbba92f588
Alok905 <alokranjanjoshi07567@gmail.com>
2025-12-08 21:51:58 +0530

    did with @ConditionalOnProperty

--- projects/module1task/src/main/java/com/bakery/module1task/AppConfig.java
+++ projects/module1task/src/main/java/com/bakery/module1task/AppConfig.java
@@ -7,6 +7,7 @@ import com.bakery.module1task.syrup.ChocolateSyrup;
 import com.bakery.module1task.syrup.StrawberrySyrup;
 import com.bakery.module1task.syrup.Syrup;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -16,34 +17,35 @@ public class AppConfig {
 
     /* it'll create bean with the name "chocolateSyrup" --------- */
     @Bean
-    @Qualifier("chocolate-syrup")
+    @ConditionalOnProperty(value = "syrup.type", havingValue = "chocolate")
     public Syrup chocolateSyrup() {
         return new ChocolateSyrup();
     }
 
     /* it'll create bean with the name "strawberrySyrup" --------- */
     @Bean
-    @Qualifier("strawberry-syrup")
+    @ConditionalOnProperty(value = "syrup.type", havingValue = "strawberry")
     public Syrup strawberrySyrup() {
         return new StrawberrySyrup();
     }
 
     /* it'll create bean with the name "chocolateFrosting" --------- */
     @Bean
-    @Primary
+    @ConditionalOnProperty(value = "frosting.type", havingValue = "chocolate")
     public Frosting chocolateFrosting() {
         return new ChocolateFrosting();
     }
 
     /* it'll create bean with the name "strawberryFrosting" --------- */
     @Bean
+    @ConditionalOnProperty(value = "frosting.type", havingValue = "strawberry")
     public Frosting strawberryFrosting() {
         return new StrawberryFrosting();
     }
 
     /* the above created bean will be injected in below. the bean name will be "cakeBaker" --------- */
     @Bean
-    public CakeBaker cakeBaker(Frosting frosting, @Qualifier("strawberry-syrup") Syrup syrup) {
+    public CakeBaker cakeBaker(Frosting frosting, Syrup syrup) {
         return new CakeBaker(frosting, syrup);
     }
 
